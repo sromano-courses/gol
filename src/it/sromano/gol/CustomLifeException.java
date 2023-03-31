1903b03222d59e816b6236dff35d2f0399c5ec26
sromano87 <siromano@unisa.it>
2023-03-31 18:37:31 +0200

    Initialized repo

---
+++ src/it/sromano/gol/CustomLifeException.java
@@ -0,0 +1,14 @@
+package it.sromano.gol;
+
+public class CustomLifeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CustomLifeException(String message) {
+		super(message);
+	}
+
+}
